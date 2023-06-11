d957c34ee0ce6928a1eebd0cde6747dad60a6325
Sobhi <s@>
2023-06-12 02:20:41 +0300

    FINISHED TODO 11

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -236,16 +236,15 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
     # TODO: replace with real data returned from querying the database
-    data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-    }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-    }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-    }]
+    artists = Artist.query.all()
+
+    data = []
+    for artist in artists:
+        temp_dict = {}
+        temp_dict["id"] = artist.id
+        temp_dict["name"] = artist.name
+        data.append(temp_dict)
+        
     return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
