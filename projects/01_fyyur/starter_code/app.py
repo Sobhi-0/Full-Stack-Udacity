ac283d771c3793b76966c50823699266f00faa2d
Sobhi <s@>
2023-06-12 17:37:29 +0300

    FINISHED TODO 13 added the artist search function

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -313,15 +313,30 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
+
+    # get what the user searched for
+    search_term = request.form.get('search_term', '')
+    artists = Artist.query.all()
+
+    # change to lowe case to make it "case-insensitive"
+    search = search_term.lower()
+    data = []
+    for artist in artists:
+        if search in artist.name.lower():
+            temp_dict = {}
+
+            temp_dict["id"] = artist.id
+            temp_dict["name"] = artist.name
+            temp_dict["num_upcoming_shows"] = len(Show.artist_shows(artist.id, "upcoming"))
+        
+            data.append(temp_dict)
+
     response={
-    "count": 1,
-    "data": [{
-        "id": 4,
-        "name": "Guns N Petals",
-        "num_upcoming_shows": 0,
-    }]
+    "count": len(data),
+    "data": data
     }
-    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+    
+    return render_template('pages/search_artists.html', results=response, search_term=search_term)
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
