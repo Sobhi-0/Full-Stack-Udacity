36ec0bda81f9d76b996486cbc3cab204f37b2aeb
Sobhi <s@>
2023-07-02 17:56:12 +0300

    Create an endpoint to handle GET requests for categories TODO 3 Alsoo fixing the setup_db to make it work

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -12,18 +12,20 @@ class TriviaTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
-        self.app = create_app()
-        self.client = self.app.test_client
+        # TODO: Restore the original shape??
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
+        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
+        self.app = create_app(self.database_path)
+        self.client = self.app.test_client
 
-        # binds the app to the current context
-        with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
-            # create all tables
-            self.db.create_all()
+        # setup_db(self.app, self.database_path)
+
+        # # binds the app to the current context
+        # with self.app.app_context():
+        #     self.db = SQLAlchemy()
+        #     self.db.init_app(self.app)
+        #     # create all tables
+        #     self.db.create_all()
     
     def tearDown(self):
         """Executed after reach test"""
@@ -33,6 +35,14 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_show_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+        
 
 
 # Make the tests conveniently executable
